23983b76c1630f8beffe044b7379dae8befe08e7
xingxu <89942589@qq.com>
2018-08-14 23:47:57 +0800

    七麦

---
+++ qimai/getrank.py
@@ -0,0 +1,24 @@
+#coding=utf-8
+
+from base import webbase
+from common import conf,logoutput
+class GetRank():
+    def __init__(self):
+        setup=webbase.SetUp()
+        dr=setup.web_setup()
+        self.driver=webbase.Web(dr)
+        self.cf=conf.Conf()
+        self.driver.get_url(self.cf.get_conf_data("qimai")["RankUrl"])
+        self.lg=logoutput.Logger()
+
+    def close_driver(self):
+        self.driver.driver.quit()
+
+gf=GetRank()
+try:
+    pass
+
+except Ellipsis as e:
+    gf.lg.error("执行错误！")
+finally:
+    gf.close_driver()
