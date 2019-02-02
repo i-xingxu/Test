89d1d0533fdeef9ed680d1692d658733777c3368
xingxu <89942589@qq.com>
2019-02-02 18:36:41 +0800

    安卓榜单

---
+++ qimai/androidrank.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+
+from base import webbase
+from common import conf,logoutput,getxml,mysql
+class GetAndroidRank():
+    def __init__(self):
+        setup=webbase.SetUp()
+        dr=setup.web_setup()
+        self.driver=webbase.Web(dr)
+        self.cf=conf.Conf()
+        # self.driver.get_url(self.cf.get_conf_data("qimai")["keywordlist"])
+        self.lg=logoutput.Logger()
+        self.getxml=getxml.XmlOperation()
+
+
+    def open_url(self,url):
+
+        self.driver.get_url(url)
+
+    def get_table_count(self):
+        rankTableElement=self.getxml.get_xml_data("android_rank_page","rank_table")
+        rankTable=self.driver.get_elements(rankTableElement)
+        # for r in rankTable: print(r.text)
+        return len(rankTable)
+
+
+
+if __name__=="__main__":
+    g=GetAndroidRank()
+    g.open_url("https://www.qimai.cn/rank/marketRank/market/9")
+    print(g.get_table_count())
---
+++ tests/test_androidrank.py
@@ -0,0 +1,35 @@
+#coding=utf-8
+
+import pytest
+import allure
+from qimai import androidrank
+from common import mysql
+
+
+class TestAndroidRandk():
+
+    testCase=mysql.Mysql().get_mysql_data("安卓榜单")
+    def setup_class(self):
+        pass
+
+
+    @pytest.allure.feature()
+    @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
+    @pytest.mark.parametrize("url,expect", testCase)
+    def test_android_rank(self,url,expect):
+        ar=androidrank.GetAndroidRank()
+        ar.open_url(url)
+        self.dr=ar.driver
+        res=ar.get_table_count()
+        if res>1:
+            flag="True"
+        else:
+            flag="False"
+        assert expect==flag
+
+
+
+    def teardown(self):
+        self.dr.driver.quit()
+
+
